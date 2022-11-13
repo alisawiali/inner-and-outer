51a0e93c4dd60a296d6516e61c0c66fad700a34a
iimad <alisawiali70@gmail.com>
2022-11-13 15:13:50 +0100

    innerHmtl, inner Text

---
+++ main.js
@@ -0,0 +1,13 @@
+let imad = document.getElementById('continer');
+// imad.outerHTML='<h1>hallo Imad</h1>' hier wechslen wir nur das kommplet element.......
+
+
+imad.innerHTML  ='Hallo Berlin ich bin Imad'
+// imad.innerHTML  +='Hallo Berlin ich bin Imad'
+console.log(imad);
+
+// mit plus + mach wir neue satz   zb.   imad.innerHTML  +='Hallo Berlin ich bin Imad'
+
+
+// outertext ----> imad.innerText='<p> hallo</p> hier nur das text
+// innerhtml ----> imad.oterHtml='<p> hallo</p> hier wechslt alle das elemnt 
